dbbaa365c419a6ad110c49205571a50aa62e2952
wchandler2020 <william.d.chandler1@gmail.com>
2021-06-21 12:11:35 -0400

    Added menue items to app navigation

--- app/src/main/java/com/example/mychatapplication/MainActivity.java
+++ app/src/main/java/com/example/mychatapplication/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.mychatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My Chat Application");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout=findViewById(R.id.drawlayout);
+        navigationView=findViewById(R.id.navView);
+
+        view = navigationView.inflateHeaderView(R.layout.drawer_header);
     }
 }
