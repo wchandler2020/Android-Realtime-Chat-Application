0d887937bce80cee0d8ba841fe9d1859dfb0c504
wchandler2020 <william.d.chandler1@gmail.com>
2021-06-22 12:10:54 -0400

    created method in the MainActivity to get username and profile image from Firebase and add it to the menu header

--- app/src/main/java/com/example/mychatapplication/MainActivity.java
+++ app/src/main/java/com/example/mychatapplication/MainActivity.java
@@ -6,19 +6,38 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     View view;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference mUserRef;
+    String profileImageUrlV, userNameV;
+    CircleImageView profileImageHeader;
+    TextView userNameHeader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,9 +52,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout=findViewById(R.id.drawlayout);
         navigationView=findViewById(R.id.navView);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mUserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+
         view = navigationView.inflateHeaderView(R.layout.drawer_header);
+        profileImageHeader = view.findViewById(R.id.profileImage_header);
+        userNameHeader = view.findViewById(R.id.username_header);
 
         navigationView.setNavigationItemSelectedListener((this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+        else
+        {
+            mUserRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists())
+                    {
+                        profileImageUrlV = dataSnapshot.child("profileImage").getValue().toString();
+                        userNameV = dataSnapshot.child("username").getValue().toString();
+                        Picasso.get().load(profileImageUrlV).into(profileImageHeader);
+                        userNameHeader.setText(userNameV);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(MainActivity.this, "Sorry, something went wrong.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
